7e09bc15c0645c296e6fd627440c7be9b005bfbd
nikitha <105221802+hdbsfbfbhgu@users.noreply.github.com>
2022-08-10 19:43:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var Ball, database;
+var position;
+
+
+function setup(){
+  database = firebase.database()
+
+  console.log(database);
+  createCanvas(500,500);
+
+  Ball = createSprite(250,250,10,10);
+  Ball.shapeColor = "red";
+var ballposition = database.ref("ball/position")
+// on is used to read the value
+ballposition.on("value",readPosition)
+
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+  
+}
+
+function writePosition(x,y){
+  // set is used to write the value
+ database.ref("ball/position").set({
+'x':position.x+x,
+'y':position.y+y
+ })
+
+}
+
+function readPosition(data){
+ position=data.val()
+ Ball.x=position.x;
+ Ball.y=position.y;
+ }
+
+function showError(){
+}
